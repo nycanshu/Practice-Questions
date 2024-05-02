60992b9b1be53d3dbcd47e081e900c4a271f9824
Himanshu <112685847+nycanshu@users.noreply.github.com>
2024-05-02 12:15:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Serialize and deserialize a binary tree/serialize-and-deserialize-a-binary-tree.java
@@ -0,0 +1,180 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void deletetree(Node root) {
+        if (root == null) return;
+        deletetree(root.left);
+        deletetree(root.right);
+        root.left = null;
+        root.right = null;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+
+            Tree tr = new Tree();
+            ArrayList<Integer> A = tr.serialize(root);
+            deletetree(root);
+            root = null;
+
+            Node getRoot = tr.deSerialize(A);
+            printInorder(getRoot);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the given function
+Node is as follows:
+class Tree{
+    int data;
+    Tree left,right;
+    Tree(int d){
+        data=d;
+        left=right=null;
+    }
+}*/
+
+class Tree 
+{
+    public ArrayList<Integer> serialize(Node root) 
+    {
+        ArrayList<Integer> level = new ArrayList<>();
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            int n = q.size();
+            while(n-- > 0)
+            {
+                Node p = q.remove();
+                if(p == null)
+                {
+                    level.add(-1);
+                    continue;
+                }
+                level.add(p.data);
+                q.add(p.left);
+                q.add(p.right);
+            }
+        }
+        return level;
+    }
+    public Node deSerialize(ArrayList<Integer> a) 
+    {
+        Node root = new Node(a.get(0));
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        int i = 1;
+        while(!q.isEmpty() && i < a.size()) 
+        {
+            Node p = q.remove();
+            if(a.get(i) == -1)
+                p.left = null;
+            else 
+            {
+                p.left = new Node(a.get(i));
+                q.add(p.left);
+            }
+            i++;
+            if(a.get(i) == -1)
+                p.right = null;
+            else 
+            {
+                p.right = new Node(a.get(i));
+                q.add(p.right);
+            }
+            i++;
+        }
+        return root;
+    }
+};
