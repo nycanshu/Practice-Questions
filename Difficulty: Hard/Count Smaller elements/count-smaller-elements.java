890e440a6397afdb0e94a66e6b6a80753e146994
Himanshu Kumar <112685847+nycanshu@users.noreply.github.com>
2024-07-19 00:15:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Count Smaller elements/count-smaller-elements.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int arr[] = new int[s.length];
+            for (int i = 0; i < s.length; i++) {
+                arr[i] = Integer.parseInt(s[i]);
+            }
+
+            int[] ans = new Solution().constructLowerArray(arr);
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int[] constructLowerArray(int[] arr) {
+        // code here
+        int[] ans = new int[arr.length];
+        
+        for(int i=0;i<arr.length-1;i++){
+            int val = arr[i];
+            int count=0;
+            for(int j=i+1;j<arr.length;j++){
+                if(val > arr[j]){
+                    count++;
+                }
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
+}
