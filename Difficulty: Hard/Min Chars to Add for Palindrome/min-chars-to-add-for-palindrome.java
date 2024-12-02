0f7f52f76b80be15642bf50ad02fa0ed87c892e1
Himanshu Kumar <112685847+nycanshu@users.noreply.github.com>
2024-12-03 00:11:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Min Chars to Add for Palindrome/min-chars-to-add-for-palindrome.java
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    public static int minChar(String str) {
+        // Reverse the string
+        String revStr = new StringBuilder(str).reverse().toString();
+        
+        // Concatenate the string with its reverse using a special character
+        String concat = str + "$" + revStr;
+        
+        // Compute the LPS array for the concatenated string
+        int[] lps = computeLPS(concat);
+        
+        // The number of characters to be added is the length of the original string
+        // minus the length of the longest palindromic suffix
+        return str.length() - lps[lps.length - 1];
+    }
+
+    private static int[] computeLPS(String s) {
+        int n = s.length();
+        int[] lps = new int[n];
+        int length = 0; // Length of the previous longest prefix suffix
+        int i = 1;
+
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(length)) {
+                length++;
+                lps[i] = length;
+                i++;
+            } else {
+                if (length != 0) {
+                    length = lps[length - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        var sc = new FastReader();
+        int test = sc.nextInt();
+        while (test-- > 0) {
+            String s = sc.nextString();
+            System.out.println(Solution.minChar(s));
+
+            System.out.println("~");
+        }
+    }
+
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            st = new StringTokenizer("");
+        }
+
+        private void read() throws IOException {
+            st = new StringTokenizer(br.readLine());
+        }
+
+        public String nextString() throws IOException {
+            while (!st.hasMoreTokens()) read();
+            return st.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(nextString());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(nextString());
+        }
+    }
+}
+// } Driver Code Ends
