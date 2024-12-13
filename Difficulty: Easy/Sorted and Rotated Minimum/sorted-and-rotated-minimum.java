4722c624e2e368b4f1e4c43b46e4e3a334c6e88d
Himanshu Kumar <112685847+nycanshu@users.noreply.github.com>
2024-12-13 08:46:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine());
+
+        Solution ob = new Solution(); // Instantiate the Solution object once
+
+        while (t-- > 0) {
+            String line = scanner.nextLine();
+            String[] elements = line.split(" ");
+            int[] arr = new int[elements.length]; // Changed to int[]
+
+            for (int i = 0; i < elements.length; i++) {
+                arr[i] = Integer.parseInt(elements[i]);
+            }
+
+            System.out.println(ob.findMin(arr));
+            System.out.println("~");
+        }
+        scanner.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int findMin(int[] arr) {
+        // complete the function here
+        int n = arr.length;
+         int low = 0, high = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            //if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+                // keep the minimum:
+                ans = Math.min(ans, arr[low]);
+
+                // Eliminate left half:
+                low = mid + 1;
+
+            } else { //if right part is sorted:
+
+                // keep the minimum:
+                ans = Math.min(ans, arr[mid]);
+
+                // Eliminate right half:
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
