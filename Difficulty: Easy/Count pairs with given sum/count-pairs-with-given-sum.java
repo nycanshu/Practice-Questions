e988b730dac90c7483e0f55e67ff111487cd61f7
Himanshu Kumar <112685847+nycanshu@users.noreply.github.com>
2024-12-27 15:00:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count pairs with given sum/count-pairs-with-given-sum.java
+++ Difficulty: Easy/Count pairs with given sum/count-pairs-with-given-sum.java
@@ -1,17 +1,40 @@
 //{ Driver Code Starts
-// Initial Template for Java
-
 import java.io.*;
 import java.lang.*;
 import java.util.*;
 
+
+// } Driver Code Ends
+
+class Solution {
+
+    int countPairs(int arr[], int target) {
+        int n=arr.length;
+        int count=0;
+        HashMap<Integer,Integer>ans=new HashMap<>();
+        for(int num: arr){
+            int res=target-num;
+            if(ans.containsKey(res)){
+                count+=ans.get(res);
+            }
+            ans.put(num,ans.getOrDefault(num,0)+1);
+        }
+        return count;
+    }
+}
+
+
+
+
+
+//{ Driver Code Starts.
+
 class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine().trim());
+        int t = Integer.parseInt(read.readLine());
 
         while (t-- > 0) {
-            int k = Integer.parseInt(read.readLine().trim());
             String line = read.readLine().trim();
             String[] numsStr = line.split(" ");
             int[] nums = new int[numsStr.length];
@@ -19,40 +42,14 @@ class Main {
                 nums[i] = Integer.parseInt(numsStr[i]);
             }
 
+            int target = Integer.parseInt(read.readLine());
+
             Solution obj = new Solution();
-            int ans = obj.getPairsCount(nums, k);
-            System.out.println(ans);
+
+            System.out.println(obj.countPairs(nums, target));
+
+            System.out.println("~");
         }
     }
 }
-
 // } Driver Code Ends
-
-
-// User function Template for Java
-
-class Solution {
-    int getPairsCount(int[] arr, int sum) {
-        // code here
-        // Create a HashMap to store the frequency of each number
-        HashMap<Integer, Integer> freqMap = new HashMap<>();
-        
-        int count = 0; // Count of pairs that add up to k
-        
-        // Iterate over the array
-        for (int i = 0; i < arr.length; i++) {
-            int num = arr[i];
-            int diff = sum - num; // Calculate the difference between k and the current number
-            
-            // If the difference is already present in the HashMap, increment the count
-            if (freqMap.containsKey(diff)) {
-                count += freqMap.get(diff);
-            }
-            
-            // Update the frequency of the current number in the HashMap
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
-        }
-        
-        return count; // Return the count of pairs that add up to k
-    }
-}
