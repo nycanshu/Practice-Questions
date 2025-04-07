ef08c8dfe81b9db4b28f48d48ebcba4f2ab9a3d5
Himanshu Kumar <112685847+nycanshu@users.noreply.github.com>
2025-04-07 10:23:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,69 +1,70 @@
 //{ Driver Code Starts
-import java.util.*;
 import java.io.*;
-import java.lang.*;
+import java.util.*;
 
-class DriverClass {
+class GFG {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
             int V = sc.nextInt();
             int E = sc.nextInt();
-            for (int i = 0; i < V; i++)
-                list.add(i, new ArrayList<Integer>());
+            int[][] edges = new int[E][2];
             for (int i = 0; i < E; i++) {
-                int u = sc.nextInt();
-                int v = sc.nextInt();
-                list.get(u).add(v);
+                edges[i][0] = sc.nextInt();
+                edges[i][1] = sc.nextInt();
             }
-            if (new Solution().isCyclic(V, list) == true)
-                System.out.println("1");
-            else
-                System.out.println("0");
+
+            Solution obj = new Solution();
+            boolean ans = obj.isCyclic(V, edges);
+            System.out.println(ans ? "true" : "false");
         }
+        sc.close();
     }
 }
 // } Driver Code Ends
 
 
-/*Complete the function below*/
-
 class Solution {
-    private boolean dfsCheck(int node, ArrayList<ArrayList<Integer>> adj, int vis[], int pathVis[]) {
-        vis[node] = 1; 
-        pathVis[node] = 1; 
-        
-        // traverse for adjacent nodes 
-        for(int it : adj.get(node)) {
-            // when the node is not visited 
-            if(vis[it] == 0) {
-                if(dfsCheck(it, adj, vis, pathVis) == true) 
-                    return true; 
-            }
-            // if the node has been previously visited
-            // but it has to be visited on the same path 
-            else if(pathVis[it] == 1) {
-                return true; 
-            }
+    public boolean isCyclic(int V, int[][] edges) {
+        // code here
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<V; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge : edges){
+            adj.get(edge[0]).add(edge[1]);
         }
         
-        pathVis[node] = 0; 
-        return false; 
+        boolean[] visited = new boolean[V];
+        boolean[] recStack = new boolean[V];
+        
+        for(int i=0; i<V; i++){
+            if(isCyclicUtil(i, visited, recStack, adj)){
+                return true;
+            }
+        }
+        return false;
     }
-
-    // Function to detect cycle in a directed graph.
-    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
-        int vis[] = new int[V];
-        int pathVis[] = new int[V];
+    
+    private boolean isCyclicUtil(int v, boolean[] visited, boolean[] recStack, List<List<Integer>> adj){
+        if(recStack[v]){
+            return true;
+        }
+        if(visited[v]){
+            return false;
+        }
         
-        for(int i = 0;i<V;i++) {
-            if(vis[i] == 0) {
-                if(dfsCheck(i, adj, vis, pathVis) == true) return true; 
+        visited[v] = true;
+        recStack[v] = true;
+        
+        for(int neighbor : adj.get(v)){
+            if(isCyclicUtil(neighbor, visited, recStack, adj)){
+                return true;
             }
         }
-        return false; 
+        
+        recStack[v] = false;
+        return false;
     }
 }
