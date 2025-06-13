8df7da7a2a3bf659f94ee94be5303f8392c72236
Himanshu Kumar <112685847+nycanshu@users.noreply.github.com>
2025-06-13 09:41:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.java
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.java
@@ -1,66 +1,38 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            int k = Integer.parseInt(br.readLine());
-            // Create Solution object and find closest sum
-            Solution ob = new Solution();
-            int ans = ob.KoKoEat(arr, k);
-            System.out.println(ans);
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    public static int KoKoEat( int[] piles, int H) {
-        Arrays.sort(piles);
-        int N = piles.length;
-        int low = 1;
-        int high = piles[N - 1];
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int tm = canWeEat(piles, mid);
-            if (tm <= H) {  
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+    public int kokoEat(int[] arr, int k) {
+        // code here
+        int left = 1;
+        int right = getMax(arr);
+        int result = right;
+        
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(canFinish(arr, k, mid)){
+                result = mid;
+                right = mid - 1;
+            }else{
+                left = mid + 1;
             }
         }
-        return low;  
+        
+        return result;
     }
-
-    public static int canWeEat(int[] arr, int time) {
-        int tm = 0;
-        for (int i = 0; i < arr.length; i++) {  
-            tm += Math.ceil((double) arr[i] / time);  
+    
+    private boolean canFinish(int[] arr, int k, int speed){
+        int hours = 0;
+        for(int pile : arr){
+            hours += (pile + speed - 1) / speed;
         }
-        return tm;
+        
+        return hours <= k;
+    }
+    
+    private int getMax(int[] arr){
+        int max = 0;
+        for(int num : arr){
+            max = Math.max(max, num);
+        }
+        
+        return max;
     }
 }
