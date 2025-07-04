ad37fc81a5a5505c4ee381f9ed2bdf35cdcd6ccf
Himanshu Kumar <112685847+nycanshu@users.noreply.github.com>
2025-07-05 01:12:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Score from Subarray Mins/max-score-from-subarray-mins.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSum(int arr[]) {
+        // code here
+         // Your code goes here
+        int sum = 0;
+        int maxi = arr[0];
+        for(int i = 0; i < arr.length-1; i++){
+            sum = arr[i] + arr[i+1];
+            if(sum > maxi) maxi = sum;
+        }
+        return maxi;
+    }
+}
