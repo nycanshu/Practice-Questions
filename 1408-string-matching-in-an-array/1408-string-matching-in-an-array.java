031fcd2636fbc36dc92e5f27a9c2a21748c4e085
Himanshu Kumar <112685847+nycanshu@users.noreply.github.com>
2026-08-04 21:48:45 +0530

    Time: 5 ms (30.63%), Space: 43.6 MB (82.1%) - LeetHub

---
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> ans = new ArrayList<>();
+
+        for(int i=0;i<words.length;i++){
+
+            for(int j=0;j<words.length;j++){
+                if(i == j){
+                    continue;
+                }else{
+                    if(words[j].contains(words[i])){
+                        if(!ans.contains(words[i])){
+                            ans.add(words[i]);
+                        }
+                    }
+                }
+            }
+        }
+
+        return ans;
+
+    }
+}
