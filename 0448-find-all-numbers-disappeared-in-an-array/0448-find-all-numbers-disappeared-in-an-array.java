f38846dd664969c526c248b714fb0797709d2bdd
Himanshu Kumar <112685847+nycanshu@users.noreply.github.com>
2026-08-12 23:25:29 +0530

    Time: 6 ms (83.35%), Space: 67.2 MB (64.2%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,36 +1,19 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        //inplace replacement method
-        //traverse to the array nums and make the ith values as negative saying that it is seen(negavtice)
-        // for nums[5] we will mark the 5th values which is 4th index as seen means -gative. 
+        List<Integer> list = new ArrayList<>();
 
-        ArrayList<Integer> list = new ArrayList<>();
-
-        for(int i=0;i<nums.length;i++){
-            
-            //get val of i and mark ith val as negavtive (it will be 1 less as it is 0 indexed)
+        for (int i = 0; i < nums.length; i++) {
             int idx = Math.abs(nums[i]) - 1;
-
-
-            //if curr val is negavite means it is there
-            if(nums[idx] < 0){
-                continue;
-            }
-
-            
-
-           
-            nums[idx] = -1 * nums[idx];
-
-        }
-
-        //check the array for postive if there add its position which is index+1
-
-        for(int i=0;i<nums.length; i++){
-            if(nums[i] > 0){
-                list.add(i+1);
+            if (nums[idx] > 0) {
+                nums[idx] = -nums[idx];
             }
         }
-        return list; 
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                list.add(i + 1);
+            }
+        }
+        return list;
     }
 }
