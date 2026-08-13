3caadee4811d3a828ef797c7b584362b1a059b8b
Himanshu Kumar <112685847+nycanshu@users.noreply.github.com>
2026-08-13 20:19:54 +0530

    Time: 4 ms (80.87%), Space: 43.2 MB (77.43%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] expected = new int[heights.length];
+
+        for(int i=0; i<heights.length; i++){
+            expected[i] = heights[i];
+        }
+
+        Arrays.sort(expected);
+        int res = 0;
+        for(int i=0;i<heights.length;i++){
+            if(expected[i]!=heights[i]){
+                res++;
+            }
+        }
+        return res;
+    }
+}
