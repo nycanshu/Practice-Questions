cb4c8ef8a0611f8d1fcc113aaf174e6ec4eb6a05
Himanshu Kumar <112685847+nycanshu@users.noreply.github.com>
2026-08-14 23:52:01 +0530

    Time: 0 ms (100%), Space: 42.7 MB (56.24%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,19 +1,48 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] str = s.split(" ");
 
-        if(pattern.length() != str.length){
+        String[] words = s.split(" ");
+
+        if(pattern.length() != words.length)
             return false;
-        }
 
-        Map<Object , Integer > map = new HashMap<>();
+        Map<Object, Integer> map = new HashMap<>();
 
-        for(int i=0; i<pattern.length(); i++){
-            if(!Objects.equals(map.put(pattern.charAt(i) , i) , (map.put(str[i] , i)))){
+        /*
+         * Intuition:
+         *
+         * For a valid pattern, the repetition structure of characters
+         * and words must be identical.
+         *
+         * Example:
+         * pattern = "abba"
+         * words   = "dog cat cat dog"
+         *
+         * Previous occurrence indexes:
+         *
+         * a -> null, dog -> null
+         * b -> null, cat -> null
+         * b -> 1,    cat -> 1
+         * a -> 0,    dog -> 0
+         *
+         * At every position, the character and word must have been
+         * seen previously at the same index.
+         *
+         * map.put(key, i) returns the old value associated with key.
+         * If the old values differ, the pattern mapping is invalid.
+         *
+         * We use Map<Object, Integer> so that both Character and String
+         * keys can be stored in the same map.
+         */
+        for(int i = 0; i < pattern.length(); i++) {
+            if(!Objects.equals(
+                    map.put(pattern.charAt(i), i),
+                    map.put(words[i], i)
+            )) {
                 return false;
             }
         }
-        return true;
 
+        return true;
     }
 }
